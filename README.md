ac3c1f5b485e45a7eb536b87e3c9a82b3b9e1dbc
Parsa Asgari <driftwood64@live.com>
2023-03-01 21:35:21 +0100

    Added README.md + bootstrapping enhancement

---
+++ README.md
@@ -0,0 +1,73 @@
+# Setting Up the A2billing Stack In K8s
+
+This document is an effort to present an overview of the A2billing stack in Kubernetes.
+
+The rest of the document is organized as follows:
+
+1. Deployment Details
+   - Details about the deployment.
+2. Bootstrapping Details
+   - An overview of the Bootstrapping process.
+
+## Deployment Details
+
+### Helm Charts
+
+According to the assignment, the stack should be divided into two Helm Charts. Therefore, there are two Helm Charts in the deployment:
+
+- [a2billing-mysql-phpmyadmin](https://github.com/parsa-asgari/a2billing-docker/tree/dev/helm/a2billing-mysql-phpmyadmin)
+  - The chart that contains MySQL and phpMyAdmin
+- [a2billing-web-sip](https://github.com/parsa-asgari/a2billing-docker/tree/dev/helm/a2billing-web-sip)
+  - The chart that contains the Admin and Customer web apps along the Asterisk Sip Server
+  - Depends on [a2billing-mysql-phpmyadmin](https://github.com/parsa-asgari/a2billing-docker/tree/dev/helm/a2billing-mysql-phpmyadmin) (Ver 0.1.11)
+
+### Ingress Paths
+
+As noted, there are three main Ingress paths:
+
+- http://cluster/admin/
+- http://cluster/customer/
+- sip://cluster:5060 (TCP)
+
+### Notes:
+
+Here are the things that are not yet done:
+
+- UDP ingress for sip-server
+- Thorough testing of the sip server. However, Telnet connection currently works.
+- The phpMyAdmin, Admin, and Customer panels work as intended. However, signing up inside the Customer panel did not work, even though I did enable the signup functionality in the Admin panel. 
+  - It didn't seem like a deployment issue. More like a software bug.
+
+
+
+## Bootstrapping Details
+
+In order to bootstrap the cluster, it is advised to do the following steps:
+
+1. Clone the Git repo:
+
+   ```bash
+   git clone --recurse-submodules -j8 https://github.com/parsa-asgari/a2billing-docker
+   ```
+
+2. Change the working directory:
+
+   ```bash
+   cd a2billing-docker/helm
+   ```
+
+3. Run the shell script:
+
+   ```bash
+   bash run_bootstrap_minikube.sh
+   ```
+
+### Notes:
+
+- The script assumes you have Minikube installed. Running on a different Kubernetes cluster is not tested.
+- The script installs ChartMuseum on your machine to help resolve the [a2billing-web-sip](https://github.com/parsa-asgari/a2billing-docker/tree/dev/helm/a2billing-web-sip)'s chart dependecy.
+  - Please do note that the script runs ChartMuseum inside a Screen session. 
+- The machine which the script was tested on had a slow HDD. Therefore, there is an option to choose your storage upon bootstrapping. Please choose the relevant storage type when prompted.
+- The rest of the bootstrapping script will populate the MySQL database with the mya2billing database which is needed by A2billing.
+- 
+
--- helm/run_bootstrap_minikube.sh
+++ helm/run_bootstrap_minikube.sh
@@ -1,17 +1,50 @@
 #!/bin/bash
-echo "==> Assuming the a2billing-web-sip stack is not up and running..."
-echo "==> Bilding the a2billing-web-sip helm chart dependencies"
+
+echo "*****************************"
+echo " ----- Bootstrapping the A2billing Kubernetes Stack ----- " 
+echo ""
+echo " *** ===> Assuming the existence of a Minikube Cluster...."
+echo ""
+
+echo "Please do make sure that your working directory is inside a2billing-docker/helm/"
+echo ""
+
+echo "==> The current working directory is"
+echo $(echo $PWD)
+echo ""
+
+
+echo " ====> Installing ChartMuseum and running it on port 8080..."
+echo " ====> Please do note that for the sake of simplicity, ChartMuseum is being ran inside a Screen session..."
+curl https://raw.githubusercontent.com/helm/chartmuseum/main/scripts/get-chartmuseum | bash
+screen -d -m chartmuseum --debug --port=8080   --storage="local"   --storage-local-rootdir="./chartstorage"
+echo ""
+
+
+minikube_ip=$(minikube ip)
+if [ ! $(cat /etc/hosts  | grep ${minikube_ip}) == "" ];
+then
+echo "======> Adding cluster entry to /etc/hosts (for Ingress)"
+echo "======> Running echo '${minikube_ip}  cluster' >> /etc/hosts"
+sudo echo "${minikube_ip}  cluster" >> /etc/hosts
+else
+echo "======> cluster entry already exists in /etc/hosts"
+fi
+echo ""
+
+echo "======> Assuming the a2billing-web-sip stack is not up and running..."
+echo "======> Bilding the a2billing-web-sip helm chart dependencies"
 echo ""
 cd a2billing-web-sip
 helm dependency build
 cd ../
 echo ""
-echo "====> Running: helm install a2billing-web-sip ./a2billing-web-sip/"
+echo "======> Running: helm install a2billing-web-sip ./a2billing-web-sip/"
 echo ""
 helm install a2billing-web-sip ./a2billing-web-sip/
 echo ""
 echo ""
-echo "====> What type is the storage of your machine? (HDD or SSD)"
+echo "======> What type is the storage of your machine? (HDD or SSD)"
 echo -n "->"
 read STORAGE
 echo ""
@@ -29,13 +62,13 @@ case $STORAGE in
     ;;
 esac
 
-echo "====> Sleeping for ${SLEEP} seconds. To make sure the helm stack is up and running..."
+echo "======> Sleeping for ${SLEEP} seconds. To make sure the helm stack is up and running..."
 sleep $SLEEP
 
 mysql_state=$(kubectl get pods -l name=mysql | awk 'FNR == 2 { print $3 }')
 if [ $mysql_state = "Pending" ];
 then
-    echo "==> the MySQL pod status is Pending. Restarting the stack..."
+    echo "======> the MySQL pod status is Pending. Restarting the stack..."
 	helm uninstall a2billing-web-sip
     echo ""
     echo ""
@@ -43,17 +76,26 @@ then
 	helm install a2billing-web-sip ./a2billing-web-sip/
     echo ""
     echo ""
-    echo "==> Sleeping for ${SLEEP} seconds..."
+    echo "======> Sleeping for ${SLEEP} seconds..."
 	sleep $SLEEP
 fi
 
 mysql_pod_name=$(kubectl get pods -l name=mysql | awk 'FNR == 2 { print $1 }')
 echo ""
 echo ""
-echo "===> Running: kubectl exec -it $mysql_pod_name -- bash /.docker/mysql_bootstrap.sh"
-echo "===> Here are the outputs:"
+echo "======> Running: kubectl exec -it $mysql_pod_name -- bash /.docker/mysql_bootstrap.sh"
+echo "======> Here are the outputs:"
 echo ""
 kubectl exec -it $mysql_pod_name -- bash /.docker/mysql_bootstrap.sh
-
 echo ""
+echo ""
+
+echo "======> Setting up ingress for Asterisk"
+echo "======> Running configmap patch for Nginx Ingress..."
+kubectl patch configmap tcp-services -n ingress-nginx --patch '{"data":{"5060":"default/sipserver:5060"}}'
+echo "======> Patching the Nginx Ingress Controller..."
+kubectl patch deployment ingress-nginx-controller --patch "$(cat ingress-nginx-controller-patch.yaml)" -n ingress-nginx
+
 echo "Done"
+echo "You may use the cluster via"
+echo "*****************************"
